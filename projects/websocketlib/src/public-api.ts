b285e787b67f29bfd65d2632d02d9eebc1f510f4
1769862201 <1769862201@qq.com>
2025-06-26 20:44:30 +0900

    up

--- projects/websocketlib/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of websocketlib
- */
-
-export * from './lib/websocketlib';
-export * from './lib/websocketlib-module';
-export * from './lib/websocketchatlib.component';
-export * from './lib/websocketchat.service';
-export * from './model/user';
